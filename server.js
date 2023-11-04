5bfef341478726942f2e3e57b0b899e358b1bfb7
Rashen <rashen598@gmail.com>
2023-11-04 09:26:57 +0530

    created routes for CRUD

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const app = express();
 //creating a static server
 const port = process.env.PORT || 5000;
 
+//first has the common route then it checks the routs folder and contactRoutes.js file
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+
 //listen to the port and exectue the call back function
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
