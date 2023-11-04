59b9d255b4ae779827a531b6aa43e41f63d0da4c
Rashen <rashen598@gmail.com>
2023-11-04 09:03:58 +0530

    created express server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("hello");
+//creating a express server
+const express = require("express");
+//allows to fetch the values in .env with the help of process module
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+//creating a static server
+const port = process.env.PORT || 5000;
+
+//listen to the port and exectue the call back function
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
